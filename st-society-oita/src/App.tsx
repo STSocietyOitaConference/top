c74d1ae05f95b4813bfa6f36199fb5448f7c10a1
STSocietyOitaConference <STSocietyOitaConference@gmail.com>
2023-05-18 21:38:44 +0900

    サイトmock

--- st-society-oita/src/App.tsx
+++ st-society-oita/src/App.tsx
@@ -1,25 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./css/App.css";
+import Main from "./Main";
+import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
 function App() {
+  const theme = createTheme({
+    palette: {
+      background: {
+        default: "#fcfaea",
+      },
+      primary: {
+        main: "#e86000",
+      },
+      secondary: {
+        main: "#6f5b41",
+      },
+      text: { primary: "#e86000" },
+    },
+    typography: {
+      fontFamily: ["Noto Sans JP", "sans-serif"].join(","),
+    },
+  });
+  theme.typography.h3 = {
+    fontSize: "1.2rem",
+    "@media (min-width:600px)": {
+      fontSize: "1.5rem",
+    },
+    [theme.breakpoints.up("md")]: {
+      fontSize: "2.5rem",
+    },
+  };
+  theme.typography.h4 = {
+    fontSize: "1.2rem",
+    "@media (min-width:600px)": {
+      fontSize: "1.5rem",
+    },
+    [theme.breakpoints.up("md")]: {
+      fontSize: "2rem",
+    },
+  };
+  theme.typography.h6 = {
+    fontSize: "1rem",
+    "@media (min-width:600px)": {
+      fontSize: "1rem",
+    },
+    [theme.breakpoints.up("md")]: {
+      fontSize: "1.3rem",
+    },
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Main />
+    </ThemeProvider>
   );
 }
 
